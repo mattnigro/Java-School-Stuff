643aeee56dc8a7c5688642cf8074a79795195e3d
Matthew Nigro <me@mattnigro.com>
2019-02-09 23:17:02 -0800

    HelloWorld
    Hello, C# buzzer! ("password" = password)

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = "";
+            var count = 0;
+            var name = "";
+            while (name == "")
+            {
+                Console.WriteLine("What is your name?");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine("Thanks, {0}", name);
+            while (password != "password")
+            {
+                count++;
+                if (count > 3)
+                {
+                    Console.WriteLine("No dice!");
+                    break;
+                }
+                Console.WriteLine("Authenticate your password:");
+                password = Console.ReadLine();
+            }
+            if (count > 3)
+            {
+                Console.WriteLine("Too many failed attempts");
+                Console.Beep(7734, 1000);
+                Console.Beep(3457, 1000);
+            }
+            else
+            {
+                Console.WriteLine("AUTHENTICATED: Hello {0}!", name);
+                Console.Beep(3747, 1000);
+                Console.Beep(7734, 1000);
+            }
+        }
+    }
+}
